cbb4d8b2dcd355c5444f8669670a250a5e8ded82
Lorenz Frank <lorenz@Lorenzs-MacBook-Air.local>
2026-07-02 01:12:16 +0700

    asdf

--- utils/cors.go
+++ utils/cors.go
@@ -2,38 +2,9 @@ package utils
 
 import "net/http"
 
-func EnableCORS(w http.ResponseWriter, r *http.Request) {
-
-	// =====================================================
-	// ALLOWED ORIGINS
-	// =====================================================
-	origin := r.Header.Get("Origin")
-
-	allowedOrigins := map[string]bool{
-		"http://localhost:3000":  true,
-		"https://iunoni.com":     true,
-		"https://www.iunoni.com": true,
-	}
-
-	if allowedOrigins[origin] {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-	}
-
-	// =====================================================
-	// CORS HEADERS
-	// =====================================================
-	w.Header().Set(
-		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, DELETE, OPTIONS",
-	)
-
-	w.Header().Set(
-		"Access-Control-Allow-Headers",
-		"Content-Type, Authorization",
-	)
-
-	w.Header().Set(
-		"Access-Control-Max-Age",
-		"86400",
-	)
+func EnableCORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://iunoni.com")
+	w.Header().Set("Access-Control-Allow-Origin", "http://www.iunoni.com")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 }
