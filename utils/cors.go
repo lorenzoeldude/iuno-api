19b3d71f08c707daf9e194482261abc0b49df592
Lorenz Frank <lorenz@Lorenzs-MacBook-Air.local>
2026-07-02 01:13:20 +0700

    asdf

--- utils/cors.go
+++ utils/cors.go
@@ -3,8 +3,7 @@ package utils
 import "net/http"
 
 func EnableCORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://iunoni.com")
-	w.Header().Set("Access-Control-Allow-Origin", "http://www.iunoni.com")
+	w.Header().Set("Access-Control-Allow-Origin", "https://www.iunoni.com")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 }
